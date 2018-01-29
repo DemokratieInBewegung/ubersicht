67d02f2572ab5c4d1a01aaa0a85fad185b6b1e5f
Benjamin Kampmann <ben@gnunicorn.org>
2018-01-29 22:41:31 +0100

    add facebook embed

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
         <Plenum />
         <MPNews />
         <MPNotifications />
+        <b-card>
+          <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fdemokratiebewegen%2F&tabs=timeline&height=500&small_header=true&adapt_container_width=true&hide_cover=true&show_facepile=false" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
+        </b-card>
       </b-card-group>
     </b-container>
   </div>
